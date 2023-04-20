a5564031eeb98140a06f3b36dc8f3ebeea8d554c
roman.spelina <roman.spelina@pwr.edu.pl>
2023-04-20 15:06:05 +0200

    Add import plugins

--- rentFlat/src/main.ts
+++ rentFlat/src/main.ts
@@ -12,8 +12,17 @@ import { createApp } from 'vue'
 
 // Plugins
 import { registerPlugins } from '@/plugins'
+import vuetify from "@/plugins/vuetify";
+import router from "@/router";
+import {createPinia} from "pinia";
 
 const app = createApp(App)
+
+const vue = {
+  vuetify,
+  router,
+  pinia: createPinia(),
+}
 
 registerPlugins(app)
 

f65383bfba0c059784cbdd1194d818b51d6542ed
roman.spelina <roman.spelina@pwr.edu.pl>
2023-04-21 13:28:00 +0200

    Add component change language

---
+++ rentFlat/src/components/LangSelector/ChangeLang.vue
@@ -0,0 +1,20 @@
+<template>
+<v-btn @click="toggleLanguage">dupa</v-btn>
+</template>
+
+<script lang="ts" setup>
+
+import {useI18n} from "vue-i18n";
+
+const { locale} = useI18n()
+function toggleLanguage() {
+  if (locale.value === 'en') {
+    locale.value = 'pl';
+  } else {
+    locale.value = 'en';
+  }
+  console.log(locale.value)
+}
+</script>
+<style scoped>
+</style>
